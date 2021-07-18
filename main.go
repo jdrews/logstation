a654bba2faafdd905b0a479b095368f9ec4af068
Jon Drews <jon@jondrews.com>
2021-07-17 20:02:47 -0400

    pull in a basic react app that listens to websocket and renders via react-lazylog also build single file app with statik packaged web

--- main.go
+++ main.go
@@ -1,28 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"github.com/fstab/grok_exporter/tailer/fswatcher"
 	"github.com/fstab/grok_exporter/tailer/glob"
+	"github.com/gorilla/websocket"
+	_ "github.com/jdrews/logstation/statik"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rakyll/statik/fs"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/net/websocket"
+	"net/http"
 	"os"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
 )
 
 func main() {
 
-	linesChannel := make(chan string)
+	linesChannel := make(chan string, 500)
 	//begin watching the file
 	go follow("test/logfile.log", linesChannel)
 
 	e := echo.New()
 
+	statikFS, err := fs.New()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.Static("/", "web/")
+	h := http.FileServer(statikFS)
+
+	e.GET("/*", echo.WrapHandler(http.StripPrefix("/", h)))
 
 	// pass channel into handler
 	wsHandlerChan := func(c echo.Context) error {
@@ -35,30 +48,25 @@ func main() {
 }
 
 func wshandler(c echo.Context, linesChannel <-chan string) error {
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-		for {
-			// read from
-			select {
-			case lines := <-linesChannel:
-				err := websocket.Message.Send(ws, lines)
-				if err != nil {
-					c.Logger().Error(err)
-				}
-			default:
-				fmt.Println("no message received")
-			}
+	// Disable the following line in production. Using in development so I can `npm start` and dev the frontend
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-			// Read
-			msg := ""
-			err := websocket.Message.Receive(ws, &msg)
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	for {
+		select {
+		case lines := <-linesChannel:
+			// Write
+			err := ws.WriteMessage(websocket.TextMessage, []byte(lines))
 			if err != nil {
 				c.Logger().Error(err)
 			}
-			fmt.Printf("%s\n", msg)
 		}
-	}).ServeHTTP(c.Response(), c.Request())
-	return nil
+	}
 }
 
 func follow(path string, linesChannel chan<- string) error {
@@ -78,6 +86,8 @@ func follow(path string, linesChannel chan<- string) error {
 		case line := <-tailer.Lines():
 			logger.Debug(line.Line)
 			linesChannel <- path + ":" + line.Line
+		default:
+			continue
 		}
 	}
 }
