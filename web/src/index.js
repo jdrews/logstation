545f933ea152707730497899151854216c647d9a
Jon Drews <jon@jondrews.com>
2021-07-31 02:01:31 -0400

    First pass at conversion from vanilla JS to React

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LogViewer from './LogViewer';
 // import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LogViewer />
   </React.StrictMode>,
   document.getElementById('root')
 );
